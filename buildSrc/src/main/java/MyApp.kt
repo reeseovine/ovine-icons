4aea5b17556758495ea5e3aadcfe453b4ef1be1f
Reese Jacobson <reese@ovine.xyz>
2022-02-21 16:32:18 -0600

    version bump

--- buildSrc/src/main/java/MyApp.kt
+++ buildSrc/src/main/java/MyApp.kt
@@ -2,6 +2,6 @@
 
 object MyApp {
     const val appId = "xyz.ovine.icons"
-    const val version = 2
-    const val versionName = "0.0.2"
+    const val version = 3
+    const val versionName = "0.0.3"
 }
